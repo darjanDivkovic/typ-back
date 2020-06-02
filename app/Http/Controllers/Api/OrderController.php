ec5f43937d948cb68e9740044b5ce0e660275295
Darjan Divkovic <daarjan.tz@gmail.com>
2020-06-02 11:36:17 +0200

    Orders now stored in db with api

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Order;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function store(Request $request){
+        // Create order
+        $order = Order::create($request->all());
+
+        return 'Succesfull!';
+    }
+}
